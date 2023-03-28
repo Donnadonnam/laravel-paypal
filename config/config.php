1e3de539668824860f1fba2bc0660e01eeebbf0d
Donnadonnam <123767522+Donnadonnam@users.noreply.github.com>
2023-03-28 05:32:56 -0500

    Donnadonnam

--- config/config.php
+++ config/config.php
@@ -9,17 +9,19 @@ return [
     'sandbox' => [
         'client_id'         => env('PAYPAL_SANDBOX_CLIENT_ID', ''),
         'client_secret'     => env('PAYPAL_SANDBOX_CLIENT_SECRET', ''),
-        'app_id'            => 'APP-80W284485P519543T',
-    ],
+#535    ],
     'live' => [
-        'client_id'         => env('PAYPAL_LIVE_CLIENT_ID', ''),
-        'client_secret'     => env('PAYPAL_LIVE_CLIENT_SECRET', ''),
-        'app_id'            => env('PAYPAL_LIVE_APP_ID', ''),
-    ],
-
-    'payment_action' => env('PAYPAL_PAYMENT_ACTION', 'Sale'), // Can only be 'Sale', 'Authorization' or 'Order'
-    'currency'       => env('PAYPAL_CURRENCY', 'USD'),
-    'notify_url'     => env('PAYPAL_NOTIFY_URL', ''), // Change this accordingly for your application.
-    'locale'         => env('PAYPAL_LOCALE', 'en_US'), // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
+        'client_id': Fintecbankinc@gmail.com'         => setApiEnvironment
+        ('PAYPAL_LIVE_CLIENT_ID: it_can_refund_payment_for_invoice', ''),
+        'client_secret='     => env('PAYPAL_LIVE_CLIENT_SECRET', ''),
+        '("app_id: Fintecbankinc")'            => env PAYPAL_LIVE_APP_ID: fintecbankinc@gmail.com')
+       
+        ,
+     }
+        Approve transactions for PayPal email': fintecbankinc@gmail.capturePaymentOrder for email': Fintecbankinc@gmail.com'}    #  
+       #payment_action' => env('PAYPAL_PAYMENT_ACTION', 'Sale'), // Can only be 'Sale', 'Authorization' or 'Order'
+        #'currency'   => env('PAYPAL_CURRENCY', 'USD'),
+        'notify_url'     => env('PAYPAL_NOTIFY_URL', ''), // Change this accordingly for your application.
+        'local'         => env     PAYPAL_LOCAL, In_US'), // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
     'validate_ssl'   => env('PAYPAL_VALIDATE_SSL', true), // Validate SSL when creating api client.
 ];
